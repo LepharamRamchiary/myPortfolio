c75c46768cfb134d68a8b42b79e0735eb3fb6614
Lepha <lepharamchiary@gmail.com>
2024-03-26 15:59:31 +0530

    change contect to contact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css"
 import Home from "./routes/Home";
 import About from "./routes/About";
-import Contect from "./routes/Contect";
+import Contact from "./routes/Contact";
 import Project from "./routes/Project";
 
 import { Route, Routes } from "react-router-dom";
@@ -14,7 +14,7 @@ function App() {
     <Routes> 
       <Route path="/" element={<Home />}/>
       <Route path="/project" element={<Project />}/>
-      <Route path="/contect" element={<Contect />}/>
+      <Route path="/contact" element={<Contact />}/>
       <Route path="/about" element={<About />}/>
     </Routes>
     </>
