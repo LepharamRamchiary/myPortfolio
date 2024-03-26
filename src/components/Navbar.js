75cd98654639528db5bbe70cdd142a97c1895f11
Lepha <lepharamchiary@gmail.com>
2024-03-26 11:14:30 +0530

    add ul and li

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,20 @@ const Navbar = () => {
         <Link to="/">
             <h1>Portfolio</h1>
         </Link>
+        <ul className="nav-menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/project">Project</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contect">Contect</Link>
+          </li>
+        </ul>
     </div>
   )
 }
