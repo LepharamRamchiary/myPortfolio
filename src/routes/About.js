daae91fa0bdd27246a65f347540a52a0202f3bc8
Lepha <lepharamchiary@gmail.com>
2024-03-27 21:10:40 +0530

    adding a footer,navbar and background image

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Herobg from '../components/Herobg';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Navbar />
+      <Herobg heading="ABOUT." text="I'm a fresh, friendly Front-End Developer eager to innovate in web development." />
+      <Footer />
+    </div>
   )
 }
 
