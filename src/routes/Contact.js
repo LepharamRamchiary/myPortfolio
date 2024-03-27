e7290a80a7e007a1879e913fa4d9522884f3bdb1
Lepha <lepharamchiary@gmail.com>
2024-03-27 21:11:27 +0530

    adding a navbar,footer and background image section

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Herobg from '../components/Herobg'
 
 const Contact = () => {
   return (
-    <div>Contect</div>
+    <div>
+      <Navbar />
+      <Herobg heading="CONTACT." text="Lets have a chat" />
+      <Footer />
+    </div>
   )
 }
 
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Herobg from '../components/Herobg'
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <div>
+      <Navbar />
+      <Herobg heading="PROJECTS." text="Some of my most recent works" />
+      <Footer />
+    </div>
   )
 }
 
