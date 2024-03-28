133910684834d527a40b17311fccfccf83293601
Lepha <lepharamchiary@gmail.com>
2024-03-28 23:20:58 +0530

    data for props

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import ProjectImage1 from "../assets/project1.jpg"
+import ProjectImage2 from "../assets/project4.jpg"
+import ProjectImage3 from "../assets/project3.jpg"
+
+const ProjectCardData = [
+    {
+        imgsrc: ProjectImage1,
+        title: "E-commerce Website Design",
+        text: "Discover, shop, and save with our diverse range of products. Conveniently browse, purchase, and enjoy swift delivery. Elevate your online shopping experience today!",
+        view: "https://www.google.com/search?q=ipl&rlz=1C1OPNX_enIN1044IN1044&oq=&gs_lcrp=EgZjaHJvbWUqCQgAECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxBFGDsYwgPSAQk4MjY1MmowajeoAgiwAgE&sourceid=chrome&ie=UTF-8 ",
+        source: "https://www.google.com/search?q=unsplash&rlz=1C1OPNX_enIN1044IN1044&oq=&gs_lcrp=EgZjaHJvbWUqCQgBECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxBFGDsYwgPSAQoyMDUwMjlqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8 "
+    },
+    {
+        imgsrc: ProjectImage2,
+        title: "Social Media Website",
+        text: "Discover, shop, and save with our diverse range of products. Conveniently browse, purchase, and enjoy swift delivery. Elevate your online shopping experience today!",
+        view: "https://www.google.com/search?q=ipl&rlz=1C1OPNX_enIN1044IN1044&oq=&gs_lcrp=EgZjaHJvbWUqCQgAECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxBFGDsYwgPSAQk4MjY1MmowajeoAgiwAgE&sourceid=chrome&ie=UTF-8 ",
+        source: "https://www.google.com/search?q=unsplash&rlz=1C1OPNX_enIN1044IN1044&oq=&gs_lcrp=EgZjaHJvbWUqCQgBECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxBFGDsYwgPSAQoyMDUwMjlqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8 "
+    },
+    {
+        imgsrc: ProjectImage3,
+        title: "HotelBooking Website",
+        text: "Discover, shop, and save with our diverse range of products. Conveniently browse, purchase, and enjoy swift delivery. Elevate your online shopping experience today!",
+        view: "https://www.google.com/search?q=ipl&rlz=1C1OPNX_enIN1044IN1044&oq=&gs_lcrp=EgZjaHJvbWUqCQgAECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxBFGDsYwgPSAQk4MjY1MmowajeoAgiwAgE&sourceid=chrome&ie=UTF-8 ",
+        source: "https://www.google.com/search?q=unsplash&rlz=1C1OPNX_enIN1044IN1044&oq=&gs_lcrp=EgZjaHJvbWUqCQgBECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxBFGDsYwgPSAQoyMDUwMjlqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8 "
+    }
+];
+
+export default ProjectCardData;
