a5b3a9b3bb6424aae0fa3bda781c01d0e2faf279
Lepha <lepharamchiary@gmail.com>
2024-03-29 16:00:31 +0530

    only for showing not hosted

---
+++ src/components/NotHost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotHost = () => {
+  return (
+    <div>
+        <h1>This website is not currently being hosted.</h1>
+    </div>
+  )
+}
+
+export default NotHost
