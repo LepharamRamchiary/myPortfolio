6cba3a1eb6edcc09ccabc2a3a479c4aa6ce1fc7e
Lepha <lepharamchiary@gmail.com>
2024-03-29 15:59:19 +0530

    adding form page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Herobg from '../components/Herobg'
+import Form from '../components/Form'
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <Herobg heading="CONTACT." text="Lets have a chat" />
+      <Form />
       <Footer />
     </div>
   )
