9aa05cb0fc8e4f29c5372ab48dd52629047cb699
Lepha <lepharamchiary@gmail.com>
2024-03-30 15:53:30 +0530

    adding resume

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -11,6 +11,7 @@ const AboutContent = () => {
             <div className='left'>
                 <h1>Who Am I?</h1>
                 <p>I'm a fresh React frontend developer, I embark on an exhilarating journey, brimming with passion and determination. Eager to learn, grow, and craft captivating user experiences that leave a lasting impression. </p>
+                <a href='https://drive.google.com/file/d/12InmlSAXI_m9kaHChVxfDaFyKaWEV7Zj/view?usp=sharing' className='btn'>resume</a>
                 <Link to="/contact">
                     <button className='btn'>Contact</button>
                 </Link>
